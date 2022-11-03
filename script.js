dc7c5c06f7ddf6df4be17f6fc4c56bbb4eac7917
Taui Silva <tauisilva@gmail.com>
2022-11-02 23:14:47 -0300

    Modo Escuro
    melhorias no modo noturno

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-const body = document.querySelector(".body");
-const header = document.querySelector(".header");
 const hamburguer = document.querySelector(".hamburguer");
 const navMenu = document.querySelector(".nav-menu");
-const icon = document.querySelector(".icon");
+
+const modoEscuro = document.querySelector("#modoEscuro");
+const html  = document.querySelector("html");
 
 hamburguer.addEventListener("click", () =>{
     hamburguer.classList.toggle('active');
     navMenu.classList.toggle('active');
 });
-icon.addEventListener("click", () =>{
-    body.classList.toggle('dark');
-    header.classList.toggle('dark');
+
+modoEscuro.addEventListener("click", () =>{
+
+    html.classList.toggle("modoEscuro");
 });
+
