175261f38dcffae6cfdce9489dc42c11d179cb35
Taui Silva <tauisilva@gmail.com>
2022-12-04 21:21:24 -0300

    Slide cards
    -os cards agora são visualizados em um slide
    -add função "leia mais" em sobre
    -alguns ajustes na responsividade

--- js/script.js
+++ js/script.js
@@ -2,17 +2,32 @@ const hamburguer = document.querySelector(".hamburguer");
 const navMenu = document.querySelector(".nav-menu");
 
 const modoClaro = document.querySelector("#modoClaro");
-const html  = document.querySelector("html");
+const html = document.querySelector("html");
 
-hamburguer.addEventListener("click", () =>{  
+hamburguer.addEventListener("click", () => {
     hamburguer.classList.toggle('active');
     navMenu.classList.toggle('active');
 })
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () =>{
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
     hamburguer.classList.remove('active');
     navMenu.classList.remove('active');
 }));
 
-modoClaro.addEventListener("click", () =>{
+modoClaro.addEventListener("click", () => {
     html.classList.toggle("modoClaro");
 })
+
+function leiaMais() {
+    var pontos = document.getElementById("pontos");
+    var maisTexto = document.getElementById("mostrar");
+    var bntLeiaMais = document.getElementById("bntLeiaMais");
+    if (pontos.style.display === "none") {
+        pontos.style.display = "inline";
+        maisTexto.style.display = "none";
+        bntLeiaMais.innerHTML = "Leia Mais";
+    } else {
+        pontos.style.display = "none";
+        maisTexto.style.display = "inline";
+        bntLeiaMais.innerHTML = "Leia Menos";
+    }
+}
