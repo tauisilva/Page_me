6edf49208f20ce4207e2850ba1eca14f90ecb869
Taui Silva <tauisilva@gmail.com>
2022-12-30 00:35:48 -0300

    Foooter
    adicionado footer
    adicionado modal para login

--- js/script.js
+++ js/script.js
@@ -1,27 +1,35 @@
-const hamburguer = document.querySelector(".hamburguer");
+// hamburger menu
+
 const navMenu = document.querySelector(".nav-menu");
-const modoClaro = document.querySelector("#modoClaro");
-const trocaImg = document.querySelector(".trocaImg");
-const html = document.querySelector("html");
-
-var imgwhite = "../img/projetos/paginadedemonstracaowhite.png";
-var imgblack = "../img/projetos/paginadedemonstracao.png";
-
+const hamburguer = document.querySelector(".hamburguer");
 hamburguer.addEventListener("click", () => {
     hamburguer.classList.toggle('active');
     navMenu.classList.toggle('active');
 })
+
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
     hamburguer.classList.remove('active');
     navMenu.classList.remove('active');
 }));
 
+// hamburger menu
+
+// tema
+const html = document.querySelector("html");
+const modoClaro = document.querySelector("#modoClaro");
 modoClaro.addEventListener("click", () => {
     html.classList.toggle("modoClaro");
-
+    
 })
-trocaImg.addEventListener("click", () => {
+// tema
 
+// trocar imagem
+var imgwhite = "../img/projetos/paginadedemonstracaowhite.png";
+var imgblack = "../img/projetos/paginadedemonstracao.png";
+
+const trocaImg = document.querySelector(".trocaImg");
+trocaImg.addEventListener("click", () => {
+    
     document.getElementById("demonstracaoIM").src = imgwhite;
     let aux = imgwhite;
     imgwhite = imgblack;
@@ -41,3 +49,27 @@ function leiaMais() {
         bntLeiaMais.innerHTML = "Leia Menos";
     }
 }
+
+// trocar imagem
+
+// modal login
+
+const getElement = (...queries) => document.querySelector(...queries);
+
+const button = getElement('.open-modal');
+const container = getElement('.modal-container');
+const modal = getElement('.modal');
+
+const activeModalClass = 'modal-show';
+
+const openModal = () => container.classList.add(activeModalClass);
+const closeModal = () => container.classList.remove(activeModalClass);
+
+button.addEventListener('click', openModal);
+container.addEventListener('click', (event) => {
+	if (modal.contains(event.target)) return;
+	
+	closeModal();
+});
+
+// modal login
